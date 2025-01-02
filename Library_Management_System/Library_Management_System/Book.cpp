326fdba2683867e2d580b813797b89035e8ba7e8
Minwoo <ymw1023@kakao.com>
2025-01-02 19:16:21 +0900

    Update Book class

---
+++ Library_Management_System/Library_Management_System/Book.cpp
@@ -0,0 +1,5 @@
+#include "Book.h"
+
+Book::Book(string title, string author):
+	title(title),
+	author(author) { }
--- Library_Management_System/Library_Management_System/Book.h
+++ Library_Management_System/Library_Management_System/Book.h
@@ -1,5 +1,12 @@
 #pragma once
+#include <string>
+
+using namespace std;
 
 class Book {
+public:
+	const string title;
+	const string author;
 
+	Book(string title, string author);
 };
